bdef8d02f311d17701691d734e20fd688061b5b3
Srinivas <seenuchaparala@gmail.com>
2022-03-15 13:13:57 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
 
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+const num3 = 29;
+const num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(10, 20);
+
+//console.log(random);
 // Write your code here
